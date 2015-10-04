463c832f03c51ba204ee06d70b31f7722a10254c
chonbash <zhdanovd@mbp-zhdanov>
2015-10-04 11:55:00 +0400

    Brute force solution

--- src/main/java/ru/hh/school/zhdanov/Round2.java
+++ src/main/java/ru/hh/school/zhdanov/Round2.java
@@ -4,9 +4,7 @@ package ru.hh.school.zhdanov;
 public class Round2 {
 
 	public static void main(String[] args) {
-		System.out.println("1. Минимальное расстояние");
-		System.out.println("Дан набор из N точек на плоскости (для простоты можно считать, что у всех точек целочисленные координаты).");
-		System.out.println("Найдите минимальное расстояние между двумя точками из этого набора.");
+	
 		
 		
 
